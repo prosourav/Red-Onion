e07b3ce491088248150d6eb12eb6801a1bbe94e3
prosourav <sourav19961010@gmail.com>
2021-05-30 11:00:11 +0530

    structure of details page is ready

--- src/components/FoodDetails/FoodDetails.js
+++ src/components/FoodDetails/FoodDetails.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router';
-import Footer from '../Footer/Footer';
+import './FoodDetails.css';
+// import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 const FoodDetails = () => {
-    const [dishdetail,setdishdetail] = useState({})
+    const [dishdetail,setdishdetail] = useState({});
+    const [Image,setImage] = useState('firstimage');
     const {id} = useParams();
     const url = 'http://localhost:8000/fooddetail'
     fetch(`${url}/${id}`)
@@ -12,23 +14,48 @@ const FoodDetails = () => {
     .then(data=>{
         setdishdetail(data);
     });
-    console.log(dishdetail.dishName);
+    console.log(dishdetail);
     return (
-        <>
-        <div>
-        <Header></Header>
+        <div className='container'>
+        <div className='row d-flex flex-wrap'>
+        <div className='col-md-6 col-sm-12'>
+            <div>
+                <h1>{dishdetail.dishName}</h1>
+                <p>{dishdetail.details}</p>
+            </div>
+            <div className='d-flex'>
+            <h2>${dishdetail.price}</h2>
+            <div className='d-flex'>
+            <button>+</button>
+            <span className='cart-value'>0</span>
+            <button>-</button>
+            </div>      
+            </div>
+            <button>Add</button>
+            <div className=''>
+         <img src={dishdetail.firstPhoto} className='photos' alt="" />
+         <img src={dishdetail.secondPhoto} className='photos' alt="" />
+            </div>
         </div>
-   
-        <div className='container d-flex flex-wrap'>
-        <div>
-        <h1>{dishdetail.dishName}</h1>
-        <p>{dishdetail.details}</p>
+
+        <div className='col-md-6 col-sm-12'>
         </div>
-        <div></div>
+      
+
+      
         </div>
-       </>
+       </div>
         
     );
 };
 
 export default FoodDetails;
+
+// description: "Best Dish you will have here"
+// details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s."
+// dishName: "Sandwich"
+// dishType: "Breakfast"
+// firstPhoto: "https://i.ibb.co/pWdTKYR/breakfast3.png"
+// price: "280"
+// secondPhoto: "https://i.ibb.co/kDtmRxh/breakfast4.png"
+// _id: "60afe43b48a5e52114d8a001"
